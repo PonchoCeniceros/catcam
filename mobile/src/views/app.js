6cb0a778118af4615a3524cfd9295926d12004f2
Giovanny Alfonso Chávez Ceniceros <gio.icc.uach@gmail.com>
2023-02-19 09:24:18 -0600

    [splash-screen] implementing personalized splash screen

--- mobile/src/views/app.js
+++ mobile/src/views/app.js
@@ -1,8 +1,16 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import CatCamera from '../components/catCamera';
 import Instructions from '../components/instructions';
+import { SplashScreen } from '@capacitor/splash-screen';
 
 const App = () => {
+  useEffect(async () => {
+    await SplashScreen.show({
+      showDuration: 5000,
+      autoHide: true,
+    });
+  }, []);
+
   const [instructionsAreFinished, setInstructionsStatus] = useState(false);
 
   const finishInstructions = () => {
