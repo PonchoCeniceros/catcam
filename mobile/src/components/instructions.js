0dddb61b12c0eb1fb10977f627af07c90c638bc5
Giovanny Alfonso Chávez Ceniceros <gio.icc.uach@gmail.com>
2023-02-18 22:23:05 -0600

    [main] little performance in carousel

--- mobile/src/components/instructions.js
+++ mobile/src/components/instructions.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import InstructionCard from './instructionCard';
-import { SHIFTS } from '../helpers/instructions';
+import '../assets/styles/layout.css';
 import step1 from '../assets/img/step1.png';
 import step2 from '../assets/img/step2.png';
+import { SHIFTS } from '../helpers/instructions';
 
 const Instructions = ({finish, children}) => {
   const [shift, setShift] = useState(SHIFTS.BEGIN);
@@ -16,36 +17,38 @@ const Instructions = ({finish, children}) => {
     <div className='relative overflow-hidden flex flex-row w-screen h-screen bg-[#472EE0] z-20'>
       {children}
       <div
-        style={{transition: "0.5s"}}
         className={shift}
+        style={{position: "absolute", transition: "0.25s"}}
       >
-        <InstructionCard action={shifting} shift={SHIFTS.NEXT} banner="next">
-          <div className='w-full h-full flex justify-center items-center px-8'>
-            <div className='text-5xl font-bold text-center text-white'>
-              Welcome to Cat cam!
+        <div className='w-[300vw] h-screen flex flex-row justify-center items-center'>
+          <InstructionCard action={shifting} shift={SHIFTS.NEXT} banner="next">
+            <div className='w-full h-full flex justify-center items-center px-8'>
+              <div className='text-5xl font-bold text-center text-white'>
+                Welcome to Cat cam!
+              </div>
             </div>
-          </div>
-        </InstructionCard>
-        <InstructionCard action={shifting} shift={SHIFTS.END} banner="next">
-          <div className='w-full h-5/6 flex justify-center items-center p-8'>
-            <img src={step1} width="130" height="330" />
-          </div>
-          <div className='w-full h-1/6 flex justify-center items-center px-8'>
-            <div className='text-3xl font-bold text-center text-white'>
-              take a picture with your camera...
+          </InstructionCard>
+          <InstructionCard action={shifting} shift={SHIFTS.END} banner="next">
+            <div className='w-full h-5/6 flex justify-center items-center p-8'>
+              <img src={step1} width="130" height="330" />
             </div>
-          </div>
-        </InstructionCard>
-        <InstructionCard action={shifting} shift={SHIFTS.HIDDEN} banner="go it">
-          <div className='w-full h-1/3 flex justify-center items-center px-8'>
-            <div className='text-3xl font-bold text-center text-white'>
-              ...and discover if it is a cat or not!
+            <div className='w-full h-1/6 flex justify-center items-center px-8'>
+              <div className='text-3xl font-bold text-center text-white'>
+                take a picture with your camera...
+              </div>
             </div>
-          </div>
-          <div className='w-full h-2/3 flex justify-center items-center p-8'>
-            <img src={step2} width="150" height="350" />
-          </div>
-        </InstructionCard>
+          </InstructionCard>
+          <InstructionCard action={shifting} shift={SHIFTS.HIDDEN} banner="go it">
+            <div className='w-full h-1/3 flex justify-center items-center px-8'>
+              <div className='text-3xl font-bold text-center text-white'>
+                ...and discover if it is a cat or not!
+              </div>
+            </div>
+            <div className='w-full h-2/3 flex justify-center items-center p-8'>
+              <img src={step2} width="150" height="350" />
+            </div>
+          </InstructionCard>
+        </div>
       </div>
     </div>
   );
--- mobile/src/helpers/instructions.js
+++ mobile/src/helpers/instructions.js
@@ -1,10 +1,8 @@
-const BASE_CLASS = "absolute w-[300vw] h-screen flex flex-row justify-center items-center"
-
 const SHIFTS = {
-  BEGIN: `${BASE_CLASS} -left-[0vw]`,
-  NEXT: `${BASE_CLASS} -left-[100vw]`,
-  END: `${BASE_CLASS} -left-[200vw]`,
-  HIDDEN: `${BASE_CLASS} hidden`, 
+  BEGIN: 'shift-begin',
+  NEXT: 'shift-next',
+  END: 'shift-end',
+  HIDDEN: 'shift-hidden', 
 }
 
-export {BASE_CLASS, SHIFTS};
+export {SHIFTS};
